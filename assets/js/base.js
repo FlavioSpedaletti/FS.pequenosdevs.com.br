72d9aca0d1d4e3ca3a79a1841035649b3ef66a9a
flaviohfs <flavio.hfs@gmail.com>
2018-07-03 11:55:00 -0300

    retirando chamadas externas desnecessárias e alterando cdn do bootstrap.css

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,5 +1,12 @@
-$("form").on("submit", function(){
-    setTimeout(function() { 
-                $('input[type=email]').val('');
-            }, 200);
-})
+// $("form").on("submit", function(){
+//     setTimeout(function() { 
+//                 $('input[type=email]').val('');
+//             }, 200);
+// })
+
+//sem jquery
+document.getElementById('mc-embedded-subscribe-form').onsubmit = function(){
+    setTimeout(function() {
+        document.getElementById('mce-email').value = '';
+    }, 200);
+}
