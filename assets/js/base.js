393d29325cf61dbc70d77aab1c27e6f74e2b82aa
Flavio Spedaletti <flavio.hfs@gmail.com>
2020-05-17 11:04:08 -0300

    Update base.js

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,10 +1,3 @@
-// $("form").on("submit", function(){
-//     setTimeout(function() { 
-//                 $('input[type=email]').val('');
-//             }, 200);
-// })
-
-//sem jquery
 document.getElementById('mc-embedded-subscribe-form').onsubmit = function(){
     setTimeout(function() {
         document.getElementById('mce-email').value = '';
